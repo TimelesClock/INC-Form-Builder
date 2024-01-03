befd7e834ef50a418e0536191d48cfa2c564a323
Timeless <leongandy027@gmail.com>
2024-01-03 21:42:29 +0800

    yes

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -108,11 +108,6 @@ const Login = () => {
                                     <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
                                         Password
                                     </label>
-                                    <div className="text-sm">
-                                        <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                                            Forgot password?
-                                        </a>
-                                    </div>
                                 </div>
                                 <div className="mt-2">
                                     <input
